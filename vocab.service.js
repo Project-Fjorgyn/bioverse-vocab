abeeba6fcf2cb743780810b9983ec46269f6942e
erebuseternal <marcelsanders96@gmail.com>
2022-03-04 13:53:30 +0000

    build tree added to vocab template

--- vocab.service.js
+++ vocab.service.js
@@ -168,3 +168,22 @@ export function LoadTaxa(path) {
     ulmus: require('../../../assets/vocab/data/trees/ulmaceae/ulmus/members.json')
   }[path];
 }
+
+export function BuildTree(name, common_name, link) {
+  var links = {
+    name,
+    common_name,
+    link,
+    members: [],
+  };
+  const members = LoadTaxa(link);
+  if (members) {
+    for (let i in members) {
+      var { name, common_name, link } = members[i];
+      if (link) {
+        links.members.push(BuildTree(name, common_name, link));
+      }
+    }
+  }
+  return links;
+}
